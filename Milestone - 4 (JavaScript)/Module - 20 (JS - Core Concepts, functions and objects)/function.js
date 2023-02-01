0092b4ba6172fc8ce495192358f4228a5c017d90
Farzana Yesmin <85834654+farzanayesminjule@users.noreply.github.com>
2023-02-01 23:56:46 +0600

    JS - Core Concepts, functions and objects
    JS - Core Concepts, functions and objects

---
+++ Milestone - 4 (JavaScript)/Module - 20 (JS - Core Concepts, functions and objects)/function.js
@@ -0,0 +1,13 @@
+function startFan(){
+    console.log("Farzana Yesmin");
+    console.log("This is my Name");
+    console.log("Also my identity");
+}
+
+console.log('..................')
+startFan();
+console.log('..................')
+startFan();
+console.log('..................')
+startFan();
+console.log('..................')
---
+++ Milestone - 4 (JavaScript)/Module - 20 (JS - Core Concepts, functions and objects)/return.js
@@ -0,0 +1,8 @@
+function add(num1, num2){
+    console.log(num1, num2);
+    var sum = num1 + num2;
+    return sum;
+}
+
+var total = add(45, 15);
+console.log('Total:',total);
