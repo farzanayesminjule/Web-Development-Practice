d7b8338b08766fc07e21e2cd06f5fe823b169018
Farzana Yesmin <85834654+farzanayesminjule@users.noreply.github.com>
2023-02-02 12:53:08 +0600

    Delete Milestone - 4 (JavaScript)/Module - 20 (JS - Core Concepts, functions and objects) directory

--- Milestone - 4 (JavaScript)/Module - 20 (JS - Core Concepts, functions and objects)/parametar.js
+++
@@ -1,9 +0,0 @@
-function startFan(name){
-    console.log("Farzana Yesmin");
-    console.log("This is my Name");
-    console.log("Also my identity");
-    console.log(name);
-
-}
-
-startFan(100);
--- Milestone - 4 (JavaScript)/Module - 20 (JS - Core Concepts, functions and objects)/return.js
+++
@@ -1,8 +0,0 @@
-function add(num1, num2){
-    console.log(num1, num2);
-    var sum = num1 + num2;
-    return sum;
-}
-
-var total = add(45, 15);
-console.log('Total:',total);
